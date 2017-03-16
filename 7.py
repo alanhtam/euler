91174ed36d0d97b613b4107611208a7344cc7f8e
alanhtam <varouet@gmail.com>
2017-03-16 16:06:13 -0400

    started problem 7

---
+++ 7.py
@@ -0,0 +1,11 @@
+# By listing the first six prime numbers, 2, 3, 5, 7, 11, 13, we can see that the
+# 6th prime number is 13.
+
+# What is the 10001st prime number?
+
+def next_prime():
+
+
+
+
+# n < p < 2n-2
